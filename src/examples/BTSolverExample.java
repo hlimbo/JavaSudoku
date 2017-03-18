638f711403842afb48e3f6ae3e7598dc3286e49f
hlimbo <limboh27@gmail.com>
2017-03-17 19:04:15 -0700

    fix merges

--- src/examples/BTSolverExample.java
+++ src/examples/BTSolverExample.java
@@ -14,32 +14,20 @@ public class BTSolverExample {
 	public static void main(String[] args)
 	{
 		//SudokuFile sf = SudokuBoardGenerator.generateBoard(9, 3, 3, 12);
-		SudokuFile SudokuFileFromFile = SudokuBoardReader.readFile("ExampleSudokuFiles/PH5.txt");
+		SudokuFile SudokuFileFromFile = SudokuBoardReader.readFile("ExampleSudokuFiles/PE1.txt");
 		BTSolver solver = new BTSolver(SudokuFileFromFile);
 		
 		
-		
-<<<<<<< HEAD
-		solver.setConsistencyChecks(ConsistencyCheck.None);
-		solver.setValueSelectionHeuristic(ValueSelectionHeuristic.None);
-		solver.setVariableSelectionHeuristic(VariableSelectionHeuristic.Degree);
-		solver.setNakedConsistency(NakedCheck.None);
-=======
 		solver.setConsistencyChecks(ConsistencyCheck.ForwardChecking);
 		solver.setValueSelectionHeuristic(ValueSelectionHeuristic.LeastConstrainingValue);
 		solver.setVariableSelectionHeuristic(VariableSelectionHeuristic.MinimumRemainingValue);
 		solver.setNakedConsistency(NakedCheck.NakedTriples);
->>>>>>> 621f5af2769065f378927506a04346c254d67ebc
 		
 		Thread t1 = new Thread(solver);
 		try
 		{
 			t1.start();
-<<<<<<< HEAD
-			t1.join(300000);//t1.join(60000);
-=======
 			t1.join(1800000);
->>>>>>> 621f5af2769065f378927506a04346c254d67ebc
 			if(t1.isAlive())
 			{
 				t1.interrupt();
